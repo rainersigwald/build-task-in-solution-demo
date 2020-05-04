ce132a997d225a7191c5f91fdcb1dc6c7660a78b
Rainer Sigwald <raines@microsoft.com>
2020-05-04 09:47:44 -0500

    Simple task implementation

--- Task/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Task
-{
-    public class Class1
-    {
-    }
-}
---
+++ Task/DemoCustomTask.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Build.Utilities;
+
+using System;
+
+namespace DemoCustomTask
+{
+    public class DemoCustomTask : Task
+    {
+        public override bool Execute()
+        {
+            Log.LogMessage(Microsoft.Build.Framework.MessageImportance.High, "Hello from a custom task!");
+
+            return !Log.HasLoggedErrors;
+        }
+    }
+}
